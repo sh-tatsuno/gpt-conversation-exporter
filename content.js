213201254a77791b88a44d70ad0bc36a4744e1a0
dondakeshimo <went.went.takkun135@gmail.com>
2023-03-27 10:41:43 +0900

    feat: extract AI and User messages properly

--- content.js
+++ content.js
@@ -1,42 +1,37 @@
+function validateMessageNode(messageNode) {
+  return messageNode.classList.contains('group');
+}
+
+function checkAISpeak(messageNode) {
+  return messageNode.querySelectorAll('.markdown').length != 0;
+}
+
+function extractUserMessage(messageNode) {
+  return messageNode.querySelector('.items-start').textContent;
+}
+
+function extractAIMessage(messageNode) {
+  return messageNode.querySelector('.markdown').textContent;
+}
+
+function extractChatUINode() {
+  return document.querySelector('.flex-col.items-center');
+}
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  console.log("hello");
     if (request.action === 'exportChat') {
-      const userMessages = Array.from(document.querySelectorAll('.items-start')).map(message => {
-        const text = message.textContent;
-        // TODO: extract proper author
-        return {author: "author", text};
-      });
+      const messages = Array.from(Array.from(extractChatUINode().children).filter(messageNode => {
+        return validateMessageNode(messageNode);
+      }).map(messageNode => {
+        if (checkAISpeak(messageNode)) {
+          const text = extractAIMessage(messageNode);
+          return {author: "AI", text};
+        } else {
+          const text = extractUserMessage(messageNode);
+          return {author: "User", text};
+        }
+      }));
 
-      const aiMessages = Array.from(document.querySelectorAll('.markdown')).map(message => {
-        const text = message.textContent;
-        return text;
-      });
-
-      // Merge and sort user and AI messages based on their position in the chat
-      // const messages = userMessages.concat(aiMessages).sort((a, b) => {
-      //   const positionA = Array.from(a.parentElement.children).indexOf(a);
-      //   const positionB = Array.from(b.parentElement.children).indexOf(b);
-      //   return positionA - positionB;
-      // });
-
-      sendResponse({ messages: userMessages });
+      sendResponse({ messages });
     }
   });
-
-
-  // const messageElements = Array.from(document.querySelectorAll('.items-start, .items-end'));
-  // const messages = messageElements.map(element => {
-  //   const userMessage = userMessages.find(msg => msg.element === element);
-  //   if (userMessage) {
-  //     return { author: 'User', text: userMessage.text };
-  //   }
-
-  //   const aiMessage = aiMessages.find(msg => msg.element === element);
-  //   if (aiMessage) {
-  //     return { author: 'AI', text: aiMessage.text };
-  //   }
-  // });
-
-  // console.log('Merged messages:', messages);
-
-  // sendResponse({ messages });
